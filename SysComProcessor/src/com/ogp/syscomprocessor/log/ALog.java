f944730176abb58e2fb9fe524c25d0a6758ecc0a
sms2000 <sms2000@runbox.com>
2014-03-01 19:51:06 +0200

    0.50
    Log changed

--- SysComProcessor/src/com/ogp/syscomprocessor/log/ALog.java
+++ SysComProcessor/src/com/ogp/syscomprocessor/log/ALog.java
@@ -14,43 +14,43 @@ public class ALog
 	
 	
 	public static void e (String 	tag, 
-				   		  String 	message) 
+	   		  			  String 	message) 
 	{
-		Log.e(mainTag, prepareLog (tag, message));
+		Log.e(prepareLog (mainTag, tag), message);
 	}
-	
-	
+
+
 	public static void w (String 	tag, 
-				   		  String 	message) 
+	   		  			  String 	message) 
 	{
-		Log.w(mainTag, prepareLog (tag, message));
+		Log.w(prepareLog (mainTag, tag), message);
 	}
-	
-	
+
+
 	public static void i (String 	tag, 
-				   		  String 	message) 
+	   		  			  String 	message) 
 	{
-		Log.i(mainTag, prepareLog (tag, message));
+		Log.i(prepareLog (mainTag, tag), message);
 	}
-	
-	
+
+
 	public static void d (String 	tag, 
-				   		  String 	message) 
+	   		  			  String 	message) 
 	{
-		Log.d(mainTag, prepareLog (tag, message));
+		Log.d(prepareLog (mainTag, tag), message);
 	}
-	
-	
+
+
 	public static void v (String 	tag, 
-				   		  String 	message) 
+	   		  			  String 	message) 
 	{
-		Log.v(mainTag, prepareLog (tag, message));
+		Log.v(prepareLog (mainTag, tag), message);
 	}
 
 
 	private static String prepareLog (String 	tag,
-									  String 	message)
+						  			  String 	subtug)
 	{
-		return "<" + tag + "> " + message;
+		return tag + "." + subtug;
 	}
 }
